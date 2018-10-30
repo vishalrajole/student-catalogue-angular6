3806c8c39b43d45b3da88cd87f71b83b5eaf66bb
VishalRajole <vishal.rajole@mobikontech.com>
2018-10-31 00:15:47 +0530

    heroku deploy updates

---
+++ src/server/config/config.js
@@ -0,0 +1,9 @@
+const env = process.env.NODE_ENV || "development";
+
+if (env === "development" || env === "test") {
+  var config = require("./config.json");
+  var envConfig = config[env];
+  Object.keys(envConfig).map(key => {
+    process.env[key] = envConfig[key];
+  });
+}
